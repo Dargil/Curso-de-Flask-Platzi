bfc26e1f2e7a4d03589bfc6009ec790de8438bc6
Dargil <jefferson.marin@correo.usa.edu.co>
2021-11-28 12:33:22 -0500

    7. Request y Response

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask,request
 
 # el nombre de la aplicacion es el nombre del archivo que se llama main.py
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world flask"
+    user_ip = request.remote_addr
+    return "Hello world platzi, tu IP es {}".format(user_ip)
