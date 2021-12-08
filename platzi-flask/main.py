c21b6b09765f2c9adf65368667ec8f94cca4c817
Dargil <jefferson.marin@correo.usa.edu.co>
2021-12-08 10:23:53 -0500

    10. Estructuras de control

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -2,6 +2,7 @@ from flask import Flask,request, make_response,redirect, render_template
 
 # el nombre de la aplicacion es el nombre del archivo que se llama main.py
 app = Flask(__name__)
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -12,5 +13,9 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip' : user_ip,
+        'todos' : todos,
+    }
     #return "Hello world platzi, tu IP es {}".format(user_ip)
-    return render_template('hello.html',user_ip=user_ip)
+    return render_template('hello.html', **context)
