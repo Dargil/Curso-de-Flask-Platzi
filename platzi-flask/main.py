063e0580f97b5bf39b0b8956a5886da591878087
Dargil <jefferson.marin@correo.usa.edu.co>
2021-12-08 11:27:20 -0500

    14. Configurar páginas de error

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -3,6 +3,16 @@ from flask import Flask,request, make_response,redirect, render_template
 # el nombre de la aplicacion es el nombre del archivo que se llama main.py
 app = Flask(__name__)
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html',error=error)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
