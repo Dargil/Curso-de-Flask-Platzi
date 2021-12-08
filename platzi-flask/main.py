c4ec1e0f16458d062218d796b128337315776521
Dargil <jefferson.marin@correo.usa.edu.co>
2021-12-08 10:06:18 -0500

    8. Ciclos de Request y Response

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -1,9 +1,15 @@
-from flask import Flask,request
+from flask import Flask,request, make_response,redirect
 
 # el nombre de la aplicacion es el nombre del archivo que se llama main.py
 app = Flask(__name__)
-
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
     return "Hello world platzi, tu IP es {}".format(user_ip)
