b53acfa31a42dd423d02be6b7e85ecf0e248cb50
Dargil <jefferson.marin@correo.usa.edu.co>
2021-12-08 11:44:02 -0500

    15. Flask Bootstrap

--- platzi-flask/main.py
+++ platzi-flask/main.py
@@ -1,7 +1,9 @@
 from flask import Flask,request, make_response,redirect, render_template
-
+from flask_bootstrap import  Bootstrap
 # el nombre de la aplicacion es el nombre del archivo que se llama main.py
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
 @app.errorhandler(404)
